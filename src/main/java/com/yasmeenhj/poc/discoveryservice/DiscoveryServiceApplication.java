33ee9dd4edecf978ae058ec3bd8ef88d4cb6255a
YasmeenAlHejoj <Yasmeen.Aspier@gmail.com>
2022-06-20 15:41:49 +0300

    init

---
+++ src/main/java/com/yasmeenhj/poc/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.yasmeenhj.poc.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * this project used to register  services
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
